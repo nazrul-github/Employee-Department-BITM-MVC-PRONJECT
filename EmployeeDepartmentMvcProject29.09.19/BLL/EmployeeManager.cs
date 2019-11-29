c2be5f5e3a65134556e7d9fb48bf80c2df4260d0
Robin <nozrulgithub@gmail.com>
2019-11-29 12:35:02 +0600

    Created BLL for every entity

---
+++ EmployeeDepartmentMvcProject29.09.19/BLL/EmployeeManager.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EmployeeDepartmentMvcProject29._09._19.BLL
+{
+    public class EmployeeManager
+    {
+    }
+}
