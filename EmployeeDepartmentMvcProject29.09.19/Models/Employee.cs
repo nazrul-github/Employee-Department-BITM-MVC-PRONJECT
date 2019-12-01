96e4f4a37c5743788e417f57c17d293cf2032156
Robin <nozrulgithub@gmail.com>
2019-12-01 12:53:46 +0600

    Updated delete function and validations

--- EmployeeDepartmentMvcProject29.09.19/Models/Employee.cs
+++ EmployeeDepartmentMvcProject29.09.19/Models/Employee.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,9 +9,12 @@ namespace EmployeeDepartmentMvcProject29._09._19.Models
     public class Employee
     {
         public int EmployeeId { get; set; }
+        [Required]
+        [RegularExpression(@"^(([A-za-z]+[\s]{1}[A-za-z]+)|([A-Za-z]+))$")]
         public string Name { get; set; }
         public string Designation { get; set; }
         public string NID { get; set; }
+        [DataType(DataType.Date,ErrorMessage = "Please enter a valid date")]
         public DateTime JoiningDate { get; set; }
         public int DepartmentId { get; set; }
         public string BloodGroup { get; set; }
